e4950fdc9c648d07b1101cb92481afca48546133
200657 <200657@e0601-17.JC-21.local>
2022-02-07 11:02:42 +0900

    課題６再提出

--- src/OtoKadai06.java
+++ src/OtoKadai06.java
@@ -21,20 +21,21 @@ public class OtoKadai06  {
 		oto1.powerOn();
 		oto2.powerOn();
 		while(true) {
-			oto1.lightRed();
-			oto2.lightGreen();
-			oto1.sleep(10);
-			oto2.sleep(10);
-			oto2.lightYellow();
-			oto2.sleep(3);
 			oto2.lightRed();
-			oto2.sleep(1);
 			oto1.lightGreen();
 			oto1.sleep(10);
 			oto1.lightYellow();
 			oto1.sleep(3);
 			oto1.lightRed();
 			oto1.sleep(1);
+			
+			oto1.lightRed();
+			oto2.lightGreen();
+			oto2.sleep(10);
+			oto2.lightYellow();
+			oto2.sleep(3);
+			oto2.lightRed();
+			oto2.sleep(1);
 			
 		}
 

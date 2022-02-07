0c275a05291cb3165d8c5018a4f17be81245778b
200657 <200657@e0601-17.JC-21.local>
2022-02-07 10:44:38 +0900

    課題４再提出

--- src/OtoKadai04.java
+++ src/OtoKadai04.java
@@ -24,10 +24,8 @@ public class OtoKadai04  {
 			oto1.lightAll();
 			oto2.lightOff();
 			oto1.sleep();
-			oto2.sleep();
 			oto1.lightOff();
 			oto2.lightAll();
-			oto1.sleep();
 			oto2.sleep();
 			
 		}
